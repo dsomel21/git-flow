5f45ea0d732ccde1512ff847b531e08084ede681
Dilraj Somel <dsomel@uwo.ca>
2018-04-26 10:22:38 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GitFlow::Application.config.secret_key_base = '24f9b58f2061be3a680b2c54920f919873b3617e4513b89f6cca65959cadb1098939db5bac762025cf03484a6ec86b1f5f536bd77bd9ff6764f632fbbe4b098a'
